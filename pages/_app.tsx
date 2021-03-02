c1f48ed9819e77b466ce987c3877cc06bb3caaa4
Markus Dethlefsen <github@markus-dethlefsen.de>
2021-03-03 00:08:13 +0100

    chore: add otion css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,12 @@
 import { AppProps } from 'next/app'
+import { hydrate, setup } from 'otion'
+
+import options from '../otion.config'
+
+if (typeof window !== 'undefined') {
+  setup(options)
+  hydrate()
+}
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
   <Component {...pageProps} />
