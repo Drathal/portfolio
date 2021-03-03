8955e76cccd644173917c9d721ff5a76185a2479
Markus Dethlefsen <markus.dethlefsen@finanzcheck.de>
2021-03-03 13:43:12 +0100

    feat: add card list for projects

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -1,5 +1,14 @@
 import { FC } from 'react'
 import Link from 'next/link'
+import ReactMarkdown from 'react-markdown'
+
+import { makeStyles } from '@material-ui/core/styles'
+import Chip from '@material-ui/core/Chip'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
 
 import { ProjectType } from '../projects/interface'
 
@@ -7,7 +16,22 @@ interface IProps {
   projects: ProjectType[]
 }
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 400
+  },
+  media: {
+    height: 300
+  },
+  chip: {
+    float: 'right',
+    margin: '1rem 0.5rem',
+    padding: '.5rem'
+  }
+})
+
 const ProjectList: FC<IProps> = ({ projects }) => {
+  const classes = useStyles()
   if (!projects) return null
 
   return (
@@ -15,10 +39,51 @@ const ProjectList: FC<IProps> = ({ projects }) => {
       <ul>
         {projects &&
           projects.map((project) => {
+            const projectlink = project.frontmatter.link
+              ? project.frontmatter.link
+              : `/project/${project.slug}`
+
+            const projecttarget = project.frontmatter.link ? '_blank' : null
+
             return (
               <li key={project.slug}>
-                <Link href={{ pathname: `/project/${project.slug}` }}>
-                  <a>{project.frontmatter.title}</a>
+                <Link
+                  href={{
+                    pathname: projectlink
+                  }}
+                >
+                  <a target={projecttarget}>
+                    <Card className={classes.root}>
+                      <CardActionArea>
+                        <Chip
+                          className={classes.chip}
+                          size="small"
+                          label={project.frontmatter.year}
+                          color="primary"
+                        />
+                        <CardMedia
+                          className={classes.media}
+                          image={`/thumbs/${project.frontmatter.thumb}`}
+                          title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                          <Typography variant="h5" component="h2">
+                            {project.frontmatter.title}
+                          </Typography>
+                          <Typography gutterBottom variant="h6" component="h3">
+                            {project.frontmatter.subtitle}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="span"
+                          >
+                            <ReactMarkdown source={project.markdownBody} />
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  </a>
                 </Link>
               </li>
             )
--- projects/adapive_survey_questback.md
+++ projects/adapive_survey_questback.md
@@ -1,14 +1,9 @@
 ---
-title: "Questionnaire - Questback"
-subtitle: "An adaptive Questionnaire with CSS only"
-year: "2013"
-thumb: "adaptive_survey_questback.jpg"
-link: "http://localhost:3000/projects/adaptive_survey_questback/index.html"
+title: 'Questionnaire - Questback'
+subtitle: 'An adaptive Questionnaire with CSS only'
+year: '2013'
+thumb: 'adaptive_survey_questback.jpg'
+link: 'http://localhost:3000/projects/adaptive_survey_questback/index.html'
 ---
 
-Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-
-- test1
-- test2
-- test3
-
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.
