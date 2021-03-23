28410b970083c6d0ecec28c018bad77d58d1ec95
Markus Dethlefsen <git@markus-dethlefsen.de>
2021-03-23 11:15:55 +0100

    refactor: split styling from timeline item

--- src/components/Timeline/Item.tsx
+++ src/components/Timeline/Item.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react'
 import Link from 'next/link'
 import ReactMarkdown from 'react-markdown/with-html'
-import { makeStyles } from '@material-ui/core'
 import Typography from '@material-ui/core/Typography'
 import TimelineItem from '@material-ui/lab/TimelineItem'
 import TimelineSeparator from '@material-ui/lab/TimelineSeparator'
@@ -14,7 +13,9 @@ import CardActionArea from '@material-ui/core/CardActionArea'
 import CardMedia from '@material-ui/core/CardMedia'
 import Album from '@material-ui/icons/Album'
 
-import { replacePills, pillsStyle } from '../../theme/pills.style'
+import { config } from '../../config'
+import { useStyles } from './Item.style'
+import { replacePills } from '../../theme/pills.style'
 
 interface IProps {
   slug: string
@@ -26,68 +27,6 @@ interface IProps {
   thumb: string
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    [theme.breakpoints.down('sm')]: {
-      flexDirection: 'column-reverse',
-      '&:nth-child(even)': {
-        flexDirection: 'column-reverse'
-      }
-    },
-    [theme.breakpoints.up('md')]: {
-      '&:nth-child(odd) .MuiTimelineItem-oppositeContent': {
-        '& ul': {
-          direction: 'rtl',
-          padding: '1rem 1.25rem 1rem 0'
-        }
-      },
-      '&:nth-child(even) .MuiTimelineItem-oppositeContent': {
-        '& ul': {
-          direction: 'ltr',
-          padding: '1rem 0 1rem 1.25rem'
-        }
-      }
-    }
-  },
-  separator: {
-    [theme.breakpoints.down('sm')]: {
-      display: 'none'
-    }
-  },
-  content: {
-    minHeight: '300px',
-    marginBottom: '4rem',
-
-    [theme.breakpoints.down('sm')]: {
-      minHeight: 'auto',
-      padding: '0.5rem 0',
-      textAlign: 'left'
-    },
-    '& ul': {
-      color: theme.palette.text.hint
-    },
-    '& li': {
-      fontSize: '0.98rem',
-      listStyleType: 'disc',
-      marginBottom: '0.5rem',
-      lineHeight: '110%'
-    }
-  },
-  contentCard: {
-    margin: '1rem 0 0',
-    [theme.breakpoints.down('sm')]: {
-      padding: '0.5rem 0'
-    }
-  },
-  year: {
-    margin: '0.75rem 0 0.5rem 0'
-  },
-  cardMedia: {
-    height: 300
-  },
-  pill: pillsStyle()
-}))
-
 export const Item: FC<IProps> = ({
   slug,
   link,
@@ -98,8 +37,8 @@ export const Item: FC<IProps> = ({
   thumb
 }) => {
   const classes = useStyles()
-  const thumbnail = `/thumbs/${thumb}`
-  const projectLink = link ? link : `/project/${slug}`
+  const thumbnail = `${config.path.thumbnail}${thumb}`
+  const projectLink = link ? link : `${config.path.project}${slug}`
   const projectTarget = link ? '_blank' : null
   const text = replacePills(markdownBody, classes.pill)
 
---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  path: {
+    thumbnail: `/thumbs/`,
+    project: `/project/`
+  }
+}
