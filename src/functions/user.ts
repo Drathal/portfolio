274de126a3a520c28bb18ebcf77cdaeaf3d1afff
Markus Dethlefsen <git@markus-dethlefsen.de>
2021-03-30 14:38:55 +0200

    feat: test lambda config

--- src/functions/user.ts
+++ src/functions/user.ts
@@ -5,15 +5,24 @@ dotenv.config()
 
 export const handler = async (
   event: APIGatewayEvent
-  // context: Context
 ): Promise<APIGatewayProxyResult> => {
   const { msg } = event.queryStringParameters
+
+  if (msg !== process.env.PASSWORD) {
+    return {
+      statusCode: 401,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ auth: false })
+    }
+  }
 
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ msg, test: process.env.PASSWORD })
+    body: JSON.stringify({ auth: true })
   }
 }
